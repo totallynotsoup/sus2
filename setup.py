41c51625916c7167f10f1b4d2c9a6e99746f7b19
yharonsoup <76849330+yharonsoup@users.noreply.github.com>
2021-09-23 17:08:55 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'sus2',
+  packages = ['sus2'],
+  version = '0.1',
+  license='MIT',
+  description = 'Useful module for operating sus objects',
+  author = 'yharonsoup',
+  author_email = 'yharonsoup@gmail.com',
+  url = 'https://github.com/yharonsoup/sus2/',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['sus', 'amogus'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
